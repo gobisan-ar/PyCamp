50814b5f51ead5a6d6c94064ab94a565402c858b
Gobisan Ananthanadarajan <88422076+gobisan14@users.noreply.github.com>
2022-06-28 00:06:34 +0530

    Commit 01_Variables

---
+++ 01_Variables/Print.py
@@ -0,0 +1,13 @@
+# Classic Hello World
+print("Hello World!")
+
+# Simple print
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# New Line
+print("Hello\nWorld!")
+
+# String Concatenation
+print("Hello" + " " + "World!")
---
+++ 01_Variables/Variables.py
@@ -0,0 +1,15 @@
+name = input("What is your name? ")
+length = len(name)
+print(length)
+
+a = input("a: ")
+b = input("b: ")
+
+c = a
+a = b
+b = c
+
+# a, b = b, a
+
+print("a: " + a)
+print("b: " + b)
