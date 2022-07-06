3dd8534390866de81b2959609d25dc85aba96015
Gobisan Ananthanadarajan <88422076+gobisan14@users.noreply.github.com>
2022-07-06 13:29:23 +0530

    Update 04-Lists

---
+++ 04-Lists/slice.py
@@ -0,0 +1,22 @@
+# List - mutable
+piano_key_list = ["a", "b", "c", "d", "e", "f", "g"]
+
+print(piano_key_list[2:5])
+
+print(piano_key_list[2:5:2])
+
+print(piano_key_list[::2])
+
+print(piano_key_list[::-1])
+
+
+# Tuple - immutable
+piano_key_tuple = ["do", "re", "mi", "fa", "so", "la", "ti"]
+
+print(piano_key_tuple[2:5])
+
+print(piano_key_tuple[2:5:2])
+
+print(piano_key_tuple[::2])
+
+print(piano_key_tuple[::-1])
